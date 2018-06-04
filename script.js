54d14cdae699daa6ac603433811119c2a047a1df
Ismam Ahmed <razzle.dark@gmail.com>
2018-06-04 04:36:52 +0000

    Task2a,Task2b,Task2c,Task4a,Task4b,Task4c

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* Task 1 */
-var heading = document.getElementId('heading')
+var heading = document.getElementById('heading')
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
 //Task 2 a
 function addText2(){
-    var task2div = document.getElementId('task2a')
+    var task2div = document.getElementById('task2a')
     var paragraph = document.createElement('p')
     paragraph.innerText = "Hello World"
     task2div.appendChild(paragraph)
@@ -15,7 +15,7 @@ textBtn.addEventListener('click', addText2)
 
 //Task 2 b
 function changeBGcolor(event){
-    var body = document.getElementByTagName('body')[0]
+    var body = document.getElementsByTagName('body')[0]
     if(event.target.innerText == "Red"){
         body.style.backgroundcolor = 'red'
     }
@@ -29,3 +29,21 @@ function redirectToGoogle(){
 }
 
 /* Task 4 using jQuery */
+
+//Task 4a
+$('#addText4').click(function(event){
+    var paragraph = $('<p>').text("Hello World");
+    $('#task4a').append(paragraph);
+});
+
+//Task 4b
+  function changeBG(event){
+      var color = event.target.innerText.toLowerCase();
+      $('body').css('background-color', color);
+  }
+  
+  //Task 4c
+  $('#sum').click(function(event){
+      var sum = Number($('#num1').val()) + Number($('#num2').val())
+      $('#answer').text(sum)
+  })
